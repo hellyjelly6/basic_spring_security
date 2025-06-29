ef7863f71fdf2ebbb75985a89a3e7ef64a233e1f
Angelina <anhelina_alekseeva_2019@mail.ru>
2025-06-29 16:48:41 +0300

    first settings security

---
+++ src/main/java/org/example/demo_autorization_with_form/DemoAutorizationWithFormApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo_autorization_with_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAutorizationWithFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAutorizationWithFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demo_autorization_with_form/DemoAutorizationWithFormApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo_autorization_with_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAutorizationWithFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
