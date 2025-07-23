34910c3961488213b9cdd55e1b7d353d18e51ec1
Angelina <anhelina_alekseeva_2019@mail.ru>
2025-07-23 19:35:56 +0300

    получение данных из юзердетеилс

--- src/main/java/org/example/demo_autorization_with_form/DemoAutorizationWithFormApplication.java
+++ src/main/java/org/example/demo_autorization_with_form/DemoAutorizationWithFormApplication.java
@@ -2,6 +2,15 @@ package org.example.demo_autorization_with_form;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoAutorizationWithFormApplication {
@@ -10,4 +19,21 @@ public class DemoAutorizationWithFormApplication {
         SpringApplication.run(DemoAutorizationWithFormApplication.class, args);
     }
 
+
+    //функциональный обработчик
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(){
+        return RouterFunctions.route()
+                .GET("api/v4/greetings", request -> {
+                    UserDetails userDetails = request.principal().map(Authentication.class::cast)
+                            .map(Authentication::getPrincipal)
+                            .map(UserDetails.class::cast)
+                            .orElseThrow();
+                    return ServerResponse.ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(Map.of("greeting", "Hello, %s! (V4)"
+                                    .formatted(userDetails.getUsername())));
+                })
+                .build();
+    }
 }
---
+++ src/main/java/org/example/demo_autorization_with_form/controller/GreetingsRestController.java
@@ -0,0 +1,47 @@
+package org.example.demo_autorization_with_form.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class GreetingsRestController {
+
+    //через SecurityContextHolder
+    @GetMapping("api/v1/greetings")
+    public ResponseEntity<Map<String, String>> getGreetings(){
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Map.of("greeting", "Hello, %s!"
+                        .formatted(userDetails.getUsername())));
+    }
+
+    //через HttpServletRequest
+    @GetMapping("api/v2/greetings")
+    public ResponseEntity<Map<String, String>> getGreetingsV2(HttpServletRequest request){
+        UserDetails userDetails = (UserDetails) ((Authentication)request.getUserPrincipal())
+                .getPrincipal();
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Map.of("greeting", "Hello, %s!"
+                        .formatted(userDetails.getUsername())));
+    }
+
+    //с помощью аннотации AuthenticationPrincipal
+    @GetMapping("api/v3/greetings")
+    public ResponseEntity<Map<String, String>> getGreetingsV3(@AuthenticationPrincipal UserDetails userDetails){
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Map.of("greeting", "Hello, %s!"
+                        .formatted(userDetails.getUsername())));
+    }
+}
